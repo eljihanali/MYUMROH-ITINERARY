129c4cd2b9efe5cc0602f3b3d58309473d152b29
eljihanali <eljihanali@gmail.com>
2026-02-10 16:29:39 +0700

    Add service worker for caching resources

---
+++ docs/docs/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("umroh-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./app.js",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
